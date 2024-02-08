d43e8736a70ed9eae52d5e2ad16ab9e292d98645
DaijiroNozaki-afk <dnozaki88888888@gmail.com>
2024-02-08 09:45:16 +0900

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,14 @@
+
+<!DOCKTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="utf-8">
+    <title>MyBrade</title>
+    <link rel="stylesheet" href="{{ url('css/style.css') }}">
+</head>
+<body>
+    <div class="container">
+        MyBrade
+    </div>
+</body>
+</html>
